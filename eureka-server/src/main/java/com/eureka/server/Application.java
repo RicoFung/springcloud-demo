3c7a4d72701bc6e3bb8c18a2fa937fbf7be88158
RicoFung <ricofungcn@icloud.com>
2018-06-06 16:55:13 +0800

    fix somthing

--- eureka-server/src/main/java/com/eureka/server/Application.java
+++ eureka-server/src/main/java/com/eureka/server/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer     // Eureka Server 标识
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})  // Spring Boot 应用标识
+@EnableEurekaServer // Eureka Server 标识
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // Spring Boot 应用标识
 public class Application
 {
 	public static void main(String[] args)
