8ebd33002d85aece73cd6f5b1922be877f086d9e
RicoFung <ricofungcn@icloud.com>
2018-06-06 11:43:16 +0800

    init commit

---
+++ eureka-server/src/main/java/com/eureka/server/Application.java
@@ -0,0 +1,16 @@
+package com.eureka.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer     // Eureka Server 标识
+@SpringBootApplication  // Spring Boot 应用标识
+public class Application
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
